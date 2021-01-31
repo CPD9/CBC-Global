e14201e46550302dd6d46239f5afd26fa9af59b7
CPD9 <dikepeter9@gmail.com>
2021-01-31 21:57:04 +0100

    Added local settings import

--- cbc/settings.py
+++ cbc/settings.py
@@ -144,6 +144,12 @@ MESSAGE_TAGS = {
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 465
 EMAIL_HOST_USER = 'dikepeter9@gmail.com'
-EMAIL_HOST_PASSWORD = 'moreLife2025'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = False
 EMAIL_USE_SSL = True
+
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
